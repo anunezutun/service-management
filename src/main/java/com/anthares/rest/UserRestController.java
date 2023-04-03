bd7c14984e737f383d853b188325cf97438dc98a
Abel N <anunez@zutun.io>
2023-04-02 19:19:10 -0500

    create project

---
+++ src/main/java/com/anthares/rest/UserRestController.java
@@ -0,0 +1,17 @@
+package com.anthares.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserRestController {
+
+  @GetMapping
+  public String listUser() {
+    return "hola";
+  }
+}
